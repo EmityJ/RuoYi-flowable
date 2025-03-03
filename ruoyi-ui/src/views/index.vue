94d484af6e769a583edc560fa0aec20b9c98a10a
YangBo <y806204605@qq.com>
2025-03-03 22:21:53 +0800

    修改系统名称

--- ruoyi-ui/src/views/index.vue
+++ ruoyi-ui/src/views/index.vue
@@ -1,85 +1,102 @@
 <template>
   <div class="app-container home">
     <el-row :gutter="20">
-      <el-col :xs="24" :sm="24" :md="12" :lg="14">
-        <el-card class="update-log">
+      <el-col :span="24">
+        <el-card class="role-guide">
           <div slot="header" class="clearfix">
-            <span>联系信息</span>
+            <span>欢迎使用劳务费报账系统</span>
           </div>
-          <div class="body">
-            <p>
-              <i class="el-icon-user-solid"></i> QQ群：
-              <a href="https://jq.qq.com/?_wv=1027&k=2zE87c2G" target="_blank"
-              > 782924350</a
-              >
-            </p>
-            <p>
-              <i class="el-icon-chat-dot-round"></i> 微信：<a
-            >Almost-2y</a
-            >
-            </p>
-            <p style="color: #f54a4a;font-size:16px">
-              <i class="el-icon-bell"></i> 说明：<a
-            >技术咨询、业务定制等其它支持可添加 微信: Almost-2y / QQ: 846249920 进行沟通交流</a
-            >
-            </p>
-            <p>
-              <i class="el-icon-shopping-bag-2"></i> 腾讯云秒杀：<a style="color: #365be4" href="https://curl.qcloud.com/W5KFkBG4" target="_blank"
-            >点我进入</a>
-            </p>
-            <p>
-              <i class="el-icon-shopping-bag-2"></i> 腾讯云服务器：<a style="color: #365be4" href="https://curl.qcloud.com/AacfyRxq" target="_blank"
-            >点我进入</a>
-            </p>
-            <p>
-              <i class="el-icon-shopping-bag-2"></i> 阿里云优惠：<a style="color: #365be4" href="https://www.aliyun.com/activity/daily/bestoffer?userCode=q2b8atsa" target="_blank"
-            >点我进入</a>
-            </p>
-            <p>
-              <i class="el-icon-shopping-bag-2"></i> 阿里云服务器：<a style="color: #365be4" href="https://www.aliyun.com/daily-act/ecs/activity_selection?userCode=q2b8atsa" target="_blank"
-            >点我进入</a>
-            </p>
+          <div class="guide-content">
+            <!-- 教师端指南 -->
+            <div v-if="userRole === '教师'" class="role-section">
+              <h3><i class="el-icon-user-solid"></i> 教师端功能指南</h3>
+              <el-collapse>
+                <el-collapse-item title="校外人员信息录入" name="1">
+                  <div class="guide-item">
+                    <p>用于录入校外合作人员或机构的基本信息：</p>
+                    <ul>
+                      <li>姓名、身份证号</li>
+                      <li>银行卡号、开户行</li>
+                      <li>手机号、工作单位</li>
+                    </ul>
+                  </div>
+                </el-collapse-item>
+                <el-collapse-item title="劳务费申请" name="2">
+                  <div class="guide-item">
+                    <p>提交劳务费申请，需填写以下信息：</p>
+                    <ul>
+                      <li>基本信息（姓名、身份证等）</li>
+                      <li>职称职务、发放标准</li>
+                      <li>应发金额等</li>
+                    </ul>
+                  </div>
+                </el-collapse-item>
+                <el-collapse-item title="账单管理" name="3">
+                  <div class="guide-item">
+                    <p>管理劳务费账单：</p>
+                    <ul>
+                      <li>查看劳务费明细</li>
+                      <li>复制已有劳务信息</li>
+                      <li>跟踪账单状态</li>
+                    </ul>
+                  </div>
+                </el-collapse-item>
+              </el-collapse>
+            </div>
+
+            <!-- 负责人端指南 -->
+            <div v-if="userRole === 'manager'" class="role-section">
+              <h3><i class="el-icon-s-check"></i> 负责人端功能指南</h3>
+              <el-collapse>
+                <el-collapse-item title="劳务费审核" name="1">
+                  <div class="guide-item">
+                    <p>审核劳务费申请：</p>
+                    <ul>
+                      <li>核对申请信息完整性</li>
+                      <li>审核申请合理性</li>
+                      <li>处理审核流程</li>
+                    </ul>
+                  </div>
+                </el-collapse-item>
+                <el-collapse-item title="经办人分派" name="2">
+                  <div class="guide-item">
+                    <p>项目经办人管理：</p>
+                    <ul>
+                      <li>分配项目经办人</li>
+                      <li>管理经办权限</li>
+                    </ul>
+                  </div>
+                </el-collapse-item>
+              </el-collapse>
+            </div>
+
+            <!-- 财务端指南 -->
+            <div v-if="userRole === '财务'" class="role-section">
+              <h3><i class="el-icon-money"></i> 财务端功能指南</h3>
+              <el-collapse>
+                <el-collapse-item title="项目管理" name="1">
+                  <div class="guide-item">
+                    <p>管理项目信息：</p>
+                    <ul>
+                      <li>录入/修改项目信息</li>
+                      <li>项目编码与名称管理</li>
+                      <li>项目金额与负责人设置</li>
+                    </ul>
+                  </div>
+                </el-collapse-item>
+                <el-collapse-item title="劳务费账单处理" name="2">
+                  <div class="guide-item">
+                    <p>账单管理功能：</p>
+                    <ul>
+                      <li>账单查询与撤销</li>
+                      <li>账单受理与打款</li>
+                      <li>状态更新与跟踪</li>
+                    </ul>
+                  </div>
+                </el-collapse-item>
+              </el-collapse>
+            </div>
           </div>
-        </el-card>
-      </el-col>
-      <el-col :xs="24" :sm="24" :md="12" :lg="10">
-        <el-card class="update-log">
-          <div slot="header" class="clearfix">
-            <span>捐赠支持</span>
-          </div>
-          <div class="body">
-            <img
-              src="https://foruda.gitee.com/images/1672215449995765124/596b46c3_2042292.png"
-              alt="donate"
-              width="100%"
-            />
-            <span style="display: inline-block; height: 30px; line-height: 30px"
-            >可以请作者喝杯咖啡以示鼓励！</span
-            >
-          </div>
-        </el-card>
-      </el-col>
-    </el-row>
-    <el-divider />
-    <el-row :gutter="20">
-      <el-col :xs="24" :sm="24" :md="12" :lg="8">
-        <el-card class="update-log">
-          <div slot="header" class="clearfix">
-            <span>流程功能列表</span>
-          </div>
-          <p>1.单节点配置表单</p>
-          <p>2.多实例会签任务</p>
-          <p>3.节点任务/执行监听器</p>
-          <p>4.动态配置任务候选人</p>
-          <p>5.其它模块优化</p>
-        </el-card>
-      </el-col>
-      <el-col :xs="24" :sm="24" :md="12" :lg="16">
-        <el-card class="update-log">
-          <div slot="header" class="clearfix">
-            <span>流程演示</span>
-          </div>
-          <img style="width: 850px" src="https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/f248dea817f74341b70f4087f425975b~tplv-k3u1fbpfcp-watermark.image?"/>
         </el-card>
       </el-col>
     </el-row>
@@ -91,77 +108,125 @@ export default {
   name: "index",
   data() {
     return {
-      // 版本号
-      version: "3.4.0",
+      userRole: "", // teacher, manager, finance
+      pendingTasks: 0,
+      completedTasks: 0
     };
   },
-  methods: {
-    goTarget(href) {
-      window.open(href, "_blank");
-    },
+  mounted() {
+    debugger
+    // 获取用户角色
+    this.getUserRole();
+    // 获取统计数据
+    this.getStatistics();
   },
+  methods: {
+    getUserRole() {
+      debugger
+      // 从store中获取用户信息
+      const roles = this.$store.getters && this.$store.getters.roles;
+      debugger
+      if (roles && roles.length > 0) {
+        // 根据角色数组判断用户类型
+        if (roles.includes('教师')) {
+          this.userRole = '教师';
+        } else if (roles.includes('负责人')) {
+          this.userRole = '负责人';
+        } else if (roles.includes('财务')) {
+          this.userRole = '财务';
+        } else {
+          this.userRole = '';
+        }
+      } else {
+        this.userRole = '';
+      }
+    },
+    getStatistics() {
+      // TODO: 获取待处理和已完成的任务数量
+      this.pendingTasks = 5;
+      this.completedTasks = 10;
+    },
+    goToNewLabor() {
+      this.$router.push('/labor/submit');
+    },
+    goToAudit() {
+      this.$router.push('/labor/audit');
+    },
+    goToProcess() {
+      this.$router.push('/labor/process');
+    }
+  }
 };
 </script>
 
 <style scoped lang="scss">
 .home {
-  blockquote {
-    padding: 10px 20px;
-    margin: 0 0 20px;
-    font-size: 17.5px;
-    border-left: 5px solid #eee;
-  }
-  hr {
+  .mt-20 {
     margin-top: 20px;
-    margin-bottom: 20px;
-    border: 0;
-    border-top: 1px solid #eee;
-  }
-  .col-item {
-    margin-bottom: 20px;
   }
 
-  ul {
-    padding: 0;
-    margin: 0;
-  }
+  .role-guide {
+    .guide-content {
+      padding: 10px;
+    }
 
-  font-family: "open sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
-  font-size: 13px;
-  color: #676a6c;
-  overflow-x: hidden;
+    .role-section {
+      margin-bottom: 20px;
 
-  ul {
-    list-style-type: none;
-  }
+      h3 {
+        color: #303133;
+        margin-bottom: 15px;
+        
+        i {
+          margin-right: 8px;
+        }
+      }
+    }
 
-  h4 {
-    margin-top: 0px;
-  }
-
-  h2 {
-    margin-top: 10px;
-    font-size: 26px;
-    font-weight: 100;
-  }
-
-  p {
-    margin-top: 10px;
-
-    b {
-      font-weight: 700;
+    .guide-item {
+      padding: 10px;
+      
+      ul {
+        padding-left: 20px;
+        li {
+          line-height: 24px;
+          color: #606266;
+        }
+      }
     }
   }
 
-  .update-log {
-    ol {
-      display: block;
-      list-style-type: decimal;
-      margin-block-start: 1em;
-      margin-block-end: 1em;
-      margin-inline-start: 0;
-      margin-inline-end: 0;
-      padding-inline-start: 40px;
+  .system-stats {
+    .stat-item {
+      display: flex;
+      align-items: center;
+      padding: 20px;
+      
+      i {
+        font-size: 40px;
+        margin-right: 15px;
+        color: #409EFF;
+      }
+
+      .stat-info {
+        .stat-number {
+          font-size: 24px;
+          font-weight: bold;
+          color: #303133;
+        }
+
+        .stat-label {
+          font-size: 14px;
+          color: #909399;
+        }
+      }
+    }
+  }
+
+  .quick-actions {
+    .el-button {
+      width: 100%;
+      margin-bottom: 10px;
     }
   }
 }
